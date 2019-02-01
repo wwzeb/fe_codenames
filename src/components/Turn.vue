a144cd6e049659df1e9829e22530e0f5cf50f61e
CalcCalcCalc <alecdsstone@gmail.com>
2019-02-01 23:57:38 +0000

    added turn component

---
+++ src/components/Turn.vue
@@ -0,0 +1,27 @@
+<template>
+  <div 
+    class="turn"
+    v-bind:class="[color]"
+    >
+      <h1>{{ teamName }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Turn',
+  props: [
+      'teamName',
+      'color'
+  ]
+}
+</script>
+
+<style>
+.red-team {
+    color: red;
+}
+.blue-team {
+    color: blue;
+}
+</style>
