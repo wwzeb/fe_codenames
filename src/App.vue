a3a7b35286c287b7a892e0f151c4840b5d0f7f30
CalcCalcCalc <alecdsstone@gmail.com>
2019-02-02 00:33:47 +0000

    random teams

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,18 @@
       <Score 
         :teamName="teams.team1.name" 
         :score="teams.team1.score" 
-        :color="'red-team'"
+        :color="teams.team1.color"
       ></Score>
+
+      <Turn
+        :turn="teams.team1.name"
+        :color="teams.team1.color"
+      ></Turn>
 
       <Score 
         :teamName="teams.team2.name" 
         :score="teams.team2.score" 
-        :color="'blue-team'"
+        :color="teams.team2.color"
       ></Score>
     </div>
 
@@ -34,6 +39,7 @@
 import GameBoard from './components/GameBoard';
 import Score from './components/Score';
 import Codename from './components/Codename';
+import Turn from './components/Turn';
 import WordsFile  from 'raw-loader!./assets/words.txt';
 
 export default {
@@ -41,23 +47,25 @@ export default {
   components: {
     GameBoard,
     Score,
-    Codename
+    Codename,
+    Turn
   },
   created: function() {
     const words = WordsFile.split('\n');
-
     let randomWords = [];
     while(randomWords.length <= 24){
       let randomNum = Math.floor(Math.random() * words.length) + 1;
       if(randomWords.indexOf(words[randomNum]) === -1) randomWords.push(words[randomNum]);
     }
-
     this.codenames = randomWords.map(word => {
       return {
         codename: word,
         type: this.selectRandomColor(this.types),
       }
     });
+    let coinFlip = Math.round(Math.random()) + 1;
+    this.teams["team" + coinFlip]["color"] = "red-team";
+    this.teams["team" + ((coinFlip % 2) + 1)]["color"] = "blue-team";
   },
   methods: {
     increaseScore: function (col) {
@@ -87,11 +95,11 @@ export default {
     return {
       teams: {
         team1 : {
-          name: 'team 1',
+          name: 'Team 1',
           score: 0
         },
         team2 : {
-          name: 'team 2',
+          name: 'Team 2',
           score: 0
         }
       },
@@ -116,6 +124,11 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
   user-select: none;
+}
+
+.turn {
+  display: flex;
+  justify-content: center;
 }
 
 .board {
